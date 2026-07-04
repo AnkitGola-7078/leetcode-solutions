1efb65fdfcc2515b64d18ccfa514d0d7127099a5
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-04 22:32:20 +0530

    Sync LeetCode solution: House Robber (Java)

--- Medium/0198_House_Robber.java
+++ Medium/0198_House_Robber.java
@@ -4,7 +4,7 @@
  * Difficulty: Medium
  * Language: Java
  * Runtime: 0 ms
- * Memory: 42.6 MB
+ * Memory: 42.8 MB
  * Synced From: LeetCode
  * Date: 2026-07-04
  */
@@ -24,8 +24,9 @@
     }
 }*/
 
-//recursive + dp
-class Solution {
+//recursive + dp(top down)
+
+/*class Solution {
     static int f(int[] nums ,int i,int dp[]){
 
         if(dp[i]!=-1) return dp[i];
@@ -35,11 +36,28 @@ class Solution {
         return dp[i];
     }
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0];
+        if(nums.length==1) return nums[0]
         int dp[]=new int[nums.length];
         Arrays.fill(dp,-1);
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
         return f(nums,nums.length-1,dp);
+    }
+}*/
+
+// bottom up dp
+class Solution {
+    
+    public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int dp[]=new int[nums.length];
+        Arrays.fill(dp,-1);
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.length;i++){
+              dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
+        }
+        return dp[nums.length-1];
     }
 }
