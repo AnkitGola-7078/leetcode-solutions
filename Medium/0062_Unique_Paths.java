632e2745fe24df6a46d707e3f18a85d2d385092d
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-04 23:45:35 +0530

    Sync LeetCode solution: Unique Paths (Java)

--- Medium/0062_Unique_Paths.java
+++ Medium/0062_Unique_Paths.java
@@ -4,7 +4,7 @@
  * Difficulty: Medium
  * Language: Java
  * Runtime: 0 ms
- * Memory: 41.8 MB
+ * Memory: 42 MB
  * Synced From: LeetCode
  * Date: 2026-07-04
  */
@@ -26,9 +26,8 @@
 
 //recursive+dp
 
-class Solution {
+/*class Solution {
     static int f(int m,int n,int r,int d,int dp[][]){
-        //if(r==m ||d==n)return 0;
         if(dp[r][d]!=-1) return dp[r][d];
         int right=f(m,n,r+1,d,dp);
         int down=f(m,n,r,d+1,dp);
@@ -54,32 +53,33 @@ class Solution {
 
         return f(m,n,0,0,dp);
     }
+}*/
+
+//bottom up
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
+        }
+
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
 }
-
-// //bottom up
-// class Solution {
-//     public int uniquePaths(int m, int n) {
-//         int dp[][]=new int[m][n];
-
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 dp[i][j]=-1;
-//             }
-//         }
-
-//         for(int i=0;i<n;i++){
-//             dp[m-1][i]=1;
-//         }
-
-//         for(int i=0;i<m;i++){
-//             dp[i][n-1]=1;
-//         }
-
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-                
-//             }
-//         }
-//     }
-// }
 
