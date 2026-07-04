00fa887f15232c3b9f2dcbaab78cfd8048b4e65f
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-04 23:27:06 +0530

    Sync LeetCode solution: Unique Paths (Java)

---
+++ Medium/0062_Unique_Paths.java
@@ -0,0 +1,57 @@
+/*
+ * Problem: Unique Paths
+ * Problem ID: 62
+ * Difficulty: Medium
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 41.9 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-04
+ */
+
+//recursive
+/*class Solution {
+    static int f(int m,int n,int r,int d){
+        if(r==m ||d==n)return 0;
+        if(r==m-1 && d==n-1)return 1;
+        int right=f(m,n,r+1,d);
+        int down=f(m,n,r,d+1);
+        return (right+down);
+    }
+    public int uniquePaths(int m, int n) {
+        return f(m,n,0,0);
+    }
+}*/
+
+
+//recursive+dp
+class Solution {
+    static int f(int m,int n,int r,int d,int dp[][]){
+        if(r==m ||d==n)return 0;
+        if(r==m-1 && d==n-1)return 1;
+        if(dp[r][d]!=-1) return dp[r][d];
+        int right=f(m,n,r+1,d,dp);
+        int down=f(m,n,r,d+1,dp);
+        dp[r][d] =(right+down);
+        return dp[r][d];
+    }
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=1;
+        }
+
+        for(int i=0;i<m;i++){
+            dp[i][n-1]=1;
+        }
+
+        return f(m,n,0,0,dp);
+    }
+}
