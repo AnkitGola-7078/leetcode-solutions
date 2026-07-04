be78863f77517b00432fa302c31f0d81aa883172
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-04 21:43:18 +0530

    Add method to append characters for subsequence
    Implemented a method to calculate the number of characters to append to string 's' to make 't' a subsequence.

--- Medium/2572_Append_Characters_to_String_to_Make_Subsequence.java
+++ Medium/2572_Append_Characters_to_String_to_Make_Subsequence.java
@@ -8,4 +8,16 @@
  * Synced From: LeetCode
  * Date: 2026-07-04
  */
-
+class Solution {
+    public int appendCharacters(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                 j++;
+            }
+            i++;
+        }
+        return t.length()-j;
+    }
+}
