2259dea4717f2a5e5351761f8df3dc7b303835b7
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-08 12:53:43 +0530

    Sync LeetCode solution: Generate Parentheses (Java)

--- Medium/0022_Generate_Parentheses.java
+++ Medium/0022_Generate_Parentheses.java
@@ -3,14 +3,14 @@
  * Problem ID: 22
  * Difficulty: Medium
  * Language: Java
- * Runtime: 13 ms
- * Memory: 52.3 MB
+ * Runtime: 3 ms
+ * Memory: 45.2 MB
  * Synced From: LeetCode
  * Date: 2026-07-08
  */
 
 class Solution {
-    static void f(int n,int ob,int cb,String ans,List<String> l,Map<String,Boolean> dp){
+    static void f(int n,int ob,int cb,String ans,List<String> l){
         if(ob>n/2 || cb>ob){
             return;
         }
@@ -18,18 +18,14 @@ class Solution {
             l.add(ans);
             return;
         }
-
-        String key = ob + "," + cb + "," + ans;
-        if (dp.containsKey(key)) return;  
-        dp.put(key, true);
                
-        f(n,ob+1,cb,ans+"(",l,dp);
-        f(n,ob,cb+1,ans+")",l,dp);
+        f(n,ob+1,cb,ans+"(",l);
+        f(n,ob,cb+1,ans+")",l);
     }
     public List<String> generateParenthesis(int n) {
         List<String> l=new ArrayList<>();
-        Map<String,Boolean> dp=new HashMap<>();
-        f(n*2,0,0,"",l,dp);
+        
+        f(n*2,0,0,"",l);
         return l;
     }
 }
