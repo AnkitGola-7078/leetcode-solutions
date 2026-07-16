8a7bb86cc29e763eda35724cf53b9a99ca4f2772
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-16 21:50:25 +0530

    Sync LeetCode solution: Partition Equal Subset Sum (Java)

--- Medium/0416_Partition_Equal_Subset_Sum.java
+++ Medium/0416_Partition_Equal_Subset_Sum.java
@@ -3,33 +3,29 @@
  * Problem ID: 416
  * Difficulty: Medium
  * Language: Java
- * Runtime: 105 ms
+ * Runtime: 90 ms
  * Memory: 72.4 MB
  * Synced From: LeetCode
  * Date: 2026-07-16
  */
 
 class Solution {
-    public static Boolean subset(int i, int s, int arr[], int t, Boolean dp[][]) {
-        if (s == t) return true;
-        if (i == arr.length || s > t) return false;
-
-        if (dp[i][s] != null) return dp[i][s];
-
-        boolean skip = subset(i + 1, s, arr, t, dp);
-        boolean take = subset(i + 1, s + arr[i], arr, t, dp);
-
-        return dp[i][s] = skip || take;
+    boolean f(int[] nums,int i,int t,Boolean[][] dp){
+        if(t==0) return true;
+        if(i==nums.length ||t<0) return false;
+        if(dp[i][t]!=null) return dp[i][t];
+        boolean take=f(nums,i+1,t-nums[i],dp);
+        boolean ntake=f(nums,i+1,t,dp);
+        dp[i][t]=take || ntake;
+        return dp[i][t];
     }
-
     public boolean canPartition(int[] nums) {
-        int s = 0;
-        for (int i : nums) s += i;
-
-        if (s % 2 != 0) return false;
-        int t = s / 2;
-
-        Boolean dp[][] = new Boolean[nums.length][t + 1];
-        return subset(0, 0, nums, t, dp);
+        int s=0;
+        for(int i=0;i<nums.length;i++){
+            s+=nums[i];
+        }
+        if(s%2!=0) return false;
+        Boolean[][] dp=new Boolean[nums.length][s/2+1];
+        return f(nums,0,s/2,dp);
     }
 }
