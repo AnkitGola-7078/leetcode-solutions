1b7f5e6cfa910ec26820af701156f9fc8227269f
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-18 22:32:15 +0530

    Sync LeetCode solution: Count Distinct Numbers on Board (Java)

---
+++ Easy/2679_Count_Distinct_Numbers_on_Board.java
@@ -0,0 +1,17 @@
+/*
+ * Problem: Count Distinct Numbers on Board
+ * Problem ID: 2679
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 42 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-18
+ */
+
+class Solution {
+    public int distinctIntegers(int n) {
+       if(n==1) return 1;
+       return n-1;
+    }
+}
