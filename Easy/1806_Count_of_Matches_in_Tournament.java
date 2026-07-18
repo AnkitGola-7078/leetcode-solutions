85e6d0b845a82beb3001a94477fdc68ef3fe07d1
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-18 22:20:10 +0530

    Sync LeetCode solution: Count of Matches in Tournament (Java)

---
+++ Easy/1806_Count_of_Matches_in_Tournament.java
@@ -0,0 +1,28 @@
+/*
+ * Problem: Count of Matches in Tournament
+ * Problem ID: 1806
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 42.1 MB
+ * Synced From: LeetCode
+ * Date: 2026-07-18
+ */
+
+class Solution {
+    public int numberOfMatches(int n) {
+        // int m=0;
+        // int t=0;
+        // while(n>0){
+        //     if(n%2==0){
+        //        m=m+(n/2);
+        //        n=n/2;
+        //     }else{
+        //         m+=(n-1)/2;
+        //         n=(n-1)/2+1;
+
+        //     }
+        // }
+        return n-1;
+    }
+}
