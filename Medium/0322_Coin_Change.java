8ff17203b1d10997381cf1d23c6cfbf658ad4356
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-19 13:37:22 +0530

    Sync LeetCode solution: Coin Change (Java)

--- Medium/0322_Coin_Change.java
+++ Medium/0322_Coin_Change.java
@@ -3,8 +3,8 @@
  * Problem ID: 322
  * Difficulty: Medium
  * Language: Java
- * Runtime: 39 ms
- * Memory: 47.2 MB
+ * Runtime: 36 ms
+ * Memory: 47.3 MB
  * Synced From: LeetCode
  * Date: 2026-07-19
  */
@@ -15,7 +15,7 @@ class Solution {
         if(t<0 ||i<0) return Integer.MAX_VALUE;
         if(dp[i][t]!=-1) return dp[i][t];
 
-        int take=f(coins,t-coins[i],i,dp);
+        int take=f(coins,t-coins[i],i,dp);//i becoue hum unlimit time coin use kr skyte h
         if(take != Integer.MAX_VALUE) take += 1; 
         int skip=f(coins,t,i-1,dp);
         dp[i][t] =Math.min(take,skip);
