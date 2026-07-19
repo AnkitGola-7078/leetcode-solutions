71ea5e464af52d7105c9b39b31e447ee5d408271
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-19 12:44:37 +0530

    Sync LeetCode solution: Climbing Stairs (Java)

--- Easy/0070_Climbing_Stairs.java
+++ Easy/0070_Climbing_Stairs.java
@@ -4,19 +4,24 @@
  * Difficulty: Easy
  * Language: Java
  * Runtime: 0 ms
- * Memory: 42.1 MB
+ * Memory: 42 MB
  * Synced From: LeetCode
  * Date: 2026-07-19
  */
 
 class Solution {
+    static int f(int n,int i,int dp[]){
+        if(i==n) return 1;
+        if(i>n) return 0;
+        if(dp[i]!=-1) return dp[i];
+        int take=f(n,i+1,dp);
+        int nottake=f(n,i+2,dp);
+        dp[i]= take+nottake;
+        return dp[i];
+    }
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        Arrays.fill(dp,-1);
+        return f(n,0,dp);
     }
 }
