7ce93b3e95aae74af663638c8a10888e2b2cc0ca
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-19 14:28:06 +0530

    Sync LeetCode solution: Coin Change (Java)

--- Medium/0322_Coin_Change.java
+++ Medium/0322_Coin_Change.java
@@ -3,29 +3,32 @@
  * Problem ID: 322
  * Difficulty: Medium
  * Language: Java
- * Runtime: 36 ms
- * Memory: 47.3 MB
+ * Runtime: 25 ms
+ * Memory: 46.5 MB
  * Synced From: LeetCode
  * Date: 2026-07-19
  */
 
 class Solution {
-    static int f(int[] coins, int t,int i,int dp[][]){
-        if(t==0) return 0;
-        if(t<0 ||i<0) return Integer.MAX_VALUE;
-        if(dp[i][t]!=-1) return dp[i][t];
-
-        int take=f(coins,t-coins[i],i,dp);//i becoue hum unlimit time coin use kr skyte h
-        if(take != Integer.MAX_VALUE) take += 1; 
-        int skip=f(coins,t,i-1,dp);
-        dp[i][t] =Math.min(take,skip);
-        return dp[i][t];
+    static int f(int[] nums, int t,int[] dp){
+        if(t == 0) return 0;                       
+        if(t < 0) return Integer.MAX_VALUE;       
+        if(dp[t]!=-1) return dp[t];
+        int ans = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++){      
+            int take = f(nums, t - nums[i],dp);       
+            if(take != Integer.MAX_VALUE) {        
+                ans = Math.min(ans, take + 1);     
+            }
+        }
+        dp[t]=ans;
+        return dp[t];
     }
     public int coinChange(int[] coins, int amount) {
-        int dp[][]=new int[coins.length][amount+1];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        int res=f(coins,amount,coins.length-1,dp);
-        return res==Integer.MAX_VALUE?-1:res;
         
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,-1);
+        int res = f(coins, amount,dp);
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 }
