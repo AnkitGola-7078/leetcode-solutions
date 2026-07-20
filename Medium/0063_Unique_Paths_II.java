eddc68248ef38917fc9c3bc3a67023747699a993
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-20 20:10:31 +0530

    Sync LeetCode solution: Unique Paths II (Java)

--- Medium/0063_Unique_Paths_II.java
+++ Medium/0063_Unique_Paths_II.java
@@ -4,35 +4,28 @@
  * Difficulty: Medium
  * Language: Java
  * Runtime: 0 ms
- * Memory: 43 MB
+ * Memory: 43.1 MB
  * Synced From: LeetCode
- * Date: 2026-07-04
+ * Date: 2026-07-20
  */
 
 class Solution {
-    static int f(int m,int n,int r,int d,int[][] obs,int dp[][]){
-        if(r>=m || d>=n) return 0;
+    static int f(int[][] obs,int m,int n,int r,int d,int dp[][]){
+        if(r==m ||d==n)return 0;
         if(obs[r][d]==1) return 0;
-        if(r==m-1 && d==n-1) return 1;
-        
+        if(r==m-1 && d==n-1 && obs[r][d]!=1) return 1;
         if(dp[r][d]!=-1) return dp[r][d];
-        int right=f(m,n,r+1,d,obs,dp);
-        int down=f(m,n,r,d+1,obs,dp);
-        dp[r][d] =(right+down);
+        dp[r][d]=f(obs,m,n,r+1,d,dp)+f(obs,m,n,r,d+1,dp);
         return dp[r][d];
     }
-    public int uniquePathsWithObstacles(int[][] obs) {
-        int m=obs.length;
-        int n=obs[0].length;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
         int dp[][]=new int[m][n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
+        for(int arr[]:dp){
+            Arrays.fill(arr,-1);
         }
 
-        return f(m,n,0,0,obs,dp);
+        return f(obstacleGrid,m,n,0,0,dp);
     }
 }
-
