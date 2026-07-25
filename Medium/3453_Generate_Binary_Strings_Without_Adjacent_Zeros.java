abe346cdde496e991b08d153f95157adf83b3c17
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-25 14:57:26 +0530

    Sync LeetCode solution: Generate Binary Strings Without Adjacent Zeros (Java)

--- Medium/3453_Generate_Binary_Strings_Without_Adjacent_Zeros.java
+++ Medium/3453_Generate_Binary_Strings_Without_Adjacent_Zeros.java
@@ -3,7 +3,7 @@
  * Problem ID: 3453
  * Difficulty: Medium
  * Language: Java
- * Runtime: 32 ms
+ * Runtime: 3 ms
  * Memory: 47.7 MB
  * Synced From: LeetCode
  * Date: 2026-07-25
@@ -12,21 +12,15 @@
 class Solution {
     static void f(int n,String curr, List<String> ans){
         if(curr.length()==n){
-            boolean f=true;
-          for(int i=1;i<curr.length();i++){
-            if((curr.charAt(i)=='0') && (curr.charAt(i-1)=='0')){
-                f=false;
-                break;
-            }
-          }
-           
-           if(f){
-            ans.add(curr);
-           }
-           return;
+            ans.add(curr); 
+            return;
         }
-        f(n,curr+"0",ans);
+
         f(n,curr+"1",ans);
+        if(curr.isEmpty()|| curr.charAt(curr.length()-1)=='1'){
+            f(n,curr+"0",ans);
+        }
+        
     }
     public List<String> validStrings(int n) {
         List<String> ans=new ArrayList<>();
