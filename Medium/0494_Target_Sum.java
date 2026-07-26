3607f5092bb09d125a4165d82acd24e8c0a110ea
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-26 12:50:58 +0530

    Sync LeetCode solution: Target Sum (Java)

--- Medium/0494_Target_Sum.java
+++ Medium/0494_Target_Sum.java
@@ -3,22 +3,28 @@
  * Problem ID: 494
  * Difficulty: Medium
  * Language: Java
- * Runtime: 557 ms
- * Memory: 42.8 MB
+ * Runtime: 107 ms
+ * Memory: 47.3 MB
  * Synced From: LeetCode
  * Date: 2026-07-26
  */
 
 class Solution {
-    static int f(int[] nums,int t,int s,int i){
-       
+    static int f(int[] nums,int t,int s,int i,Map<String,Integer> dp){
+        
         if(i==nums.length){
-            return (s==t)?1:0;
+            return (t==s)?1:0;
         }
-        return f(nums,t,s+nums[i],i+1)+f(nums,t,s-nums[i],i+1);
+        String key=i+","+s;
+        if(dp.containsKey(key)) return dp.get(key);
+        int ans=f(nums,t,s+nums[i],i+1,dp)+f(nums,t,s-nums[i],i+1,dp);
+        dp.put(key,ans);
+        return ans;
 
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return f(nums,target,0,0);
+        Map<String,Integer> dp=new HashMap<>();
+        return f(nums,target,0,0,dp);
+
     }
 }
