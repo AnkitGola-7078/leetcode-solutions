1c598f6b46b83db3330cdb8f4c9230c340535a30
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-07-27 10:57:09 +0530

    Sync LeetCode solution: House Robber II (Java)

--- Medium/0213_House_Robber_II.java
+++ Medium/0213_House_Robber_II.java
@@ -4,34 +4,31 @@
  * Difficulty: Medium
  * Language: Java
  * Runtime: 0 ms
- * Memory: 42.6 MB
+ * Memory: 43 MB
  * Synced From: LeetCode
  * Date: 2026-07-27
  */
 
 class Solution {
-    static int robin(int [] nums,int i,int j,int dp[]){
+    static int f(int nums[],int i,int j,int dp[]){
         if(i>j) return 0;
         if(i>=nums.length) return 0;
         if(dp[i]!=-1) return dp[i];
-        int t=nums[i]+robin(nums,i+2,j,dp);
-        int skip=robin(nums,i+1,j,dp);
-        return dp[i]=Math.max(t,skip);
-
+        int take=nums[i]+f(nums,i+2,j,dp);
+        int skip=f(nums,i+1,j,dp);
+        dp[i]= Math.max(take,skip);
+        return dp[i];
     }
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1) return nums[0];
-       
-       // Case 1: leave first house (rob from 1..n-1)//i-->start j->end
-        int[] dp1 = new int[n];
-        Arrays.fill(dp1, -1);
-        int c1 = robin(nums, 1, n - 1, dp1);
+        if(nums.length==1) return nums[0];
+        int dp1[]=new int[nums.length];
+        Arrays.fill(dp1,-1);
+        int s1=f(nums,0,nums.length-2,dp1);
 
-        // Case 2: leave last house (rob from 0..n-2)
-        int[] dp2 = new int[n];
-        Arrays.fill(dp2, -1);
-        int c2 = robin(nums, 0, n - 2, dp2);
-        return Math.max(c1,c2);
+        int dp2[]=new int[nums.length];
+        Arrays.fill(dp2,-1);
+        int s2=f(nums,1,nums.length-1,dp2);
+        return Math.max(s1,s2);
+
     }
 }
