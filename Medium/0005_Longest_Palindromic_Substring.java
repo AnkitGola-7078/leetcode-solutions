b3986b5630976fa5066fca30f1ea26a8a5fb7867
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-08-06 13:24:10 +0530

    Sync LeetCode solution: Longest Palindromic Substring (Java)

--- Medium/0005_Longest_Palindromic_Substring.java
+++ Medium/0005_Longest_Palindromic_Substring.java
@@ -3,8 +3,8 @@
  * Problem ID: 5
  * Difficulty: Medium
  * Language: Java
- * Runtime: 1857 ms
- * Memory: 47.1 MB
+ * Runtime: 1754 ms
+ * Memory: 43.7 MB
  * Synced From: LeetCode
  * Date: 2026-08-06
  */
@@ -26,24 +26,26 @@ class Solution {
         return true;
     }
     public String longestPalindrome(String s) {
-        String st="";
+        int st=0;
         int max=0;
+
+
         for(int i=0;i<s.length();i++){
            
             for(int j=i;j<s.length();j++){
                 if(ispali(i,j+1,s)){
-                    String sub = s.substring(i, j+1);
-                    if(max<sub.length()){
-                        max=sub.length();
-                        st=sub;
+                   // String sub = s.substring(i, j+1);
+                    if(max<j-i+1){
+                        max=j-i+1;
+                        st=i;
                         
                     }
                 }
             }
 
-       
+          
         }
-        return st;
+        return s.substring(st,st+max);
     }
 
 }
