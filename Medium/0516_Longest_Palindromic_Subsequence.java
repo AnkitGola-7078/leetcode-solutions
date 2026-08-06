26d24ddbed6f868335497ee75ce3ceae4953c6b5
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-08-06 15:53:58 +0530

    Sync LeetCode solution: Longest Palindromic Subsequence (Java)

--- Medium/0516_Longest_Palindromic_Subsequence.java
+++ Medium/0516_Longest_Palindromic_Subsequence.java
@@ -3,33 +3,25 @@
  * Problem ID: 516
  * Difficulty: Medium
  * Language: Java
- * Runtime: 39 ms
- * Memory: 64.1 MB
+ * Runtime: 46 ms
+ * Memory: 274 MB
  * Synced From: LeetCode
  * Date: 2026-08-06
  */
 
 class Solution {
-    public int longestPalindromeSubseq(String s) {
-       
-       int n=s.length();
-       int dp[][]=new int[n][n];
-       for(int i=0;i<n;i++){
-         dp[i][i]=1;
-       }
-        for(int l=2;l<=n;l++){
-            for(int i=0;i<n-l+1;i++){
-                int j=i+l-1;
-                if(s.charAt(i)==s.charAt(j)){
-                   dp[i][j]= 2+dp[i+1][j-1];
-                }else{
-                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
-                }
-       
-                
-            }
+     static int f(String s,int i,int j,Integer dp[][]){
+        if(i==j) return 1;
+        if(i>j) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        if(s.charAt(i)==s.charAt(j)){
+            return dp[i][j]=2+f(s,i+1,j-1,dp);
+        }else{
+           return dp[i][j]= Math.max(f(s,i+1,j,dp),f(s,i,j-1,dp));
         }
-        return dp[0][n-1];
-
+    }
+    public int longestPalindromeSubseq(String s) {
+        Integer dp[][]=new Integer[1001][1001];
+        return f(s,0,s.length()-1,dp);
     }
 }
