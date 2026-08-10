2590344d01bd832a855b5638c8807e3bd94bbd8f
ANKIT GOLA <ankitprajapati3785@gmail.com>
2026-08-10 19:45:16 +0530

    Sync LeetCode solution: Search in a Binary Search Tree (Java)

---
+++ Easy/0783_Search_in_a_Binary_Search_Tree.java
@@ -0,0 +1,40 @@
+/*
+ * Problem: Search in a Binary Search Tree
+ * Problem ID: 783
+ * Difficulty: Easy
+ * Language: Java
+ * Runtime: 0 ms
+ * Memory: 46.9 MB
+ * Synced From: LeetCode
+ * Date: 2026-08-10
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+       if(root==null) return null;
+       if(root.val==val) return root;
+       
+       if(root.val<val){
+          return searchBST(root.right,val);
+       }
+       if(root.val>val){
+           return searchBST(root.left,val);
+       }
+       return root;
+    }
+}
